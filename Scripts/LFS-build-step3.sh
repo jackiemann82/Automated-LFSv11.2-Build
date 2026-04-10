233bd5f3756adbb0533591f6c2b406a66cc1e716
Jackie Mann <jackiemann82@gmail.com>
2026-04-10 15:57:10 -0500

    Made corrections to step 3 script. Finalizing beta testing.

--- Scripts/LFS-build-step3.sh
+++ Scripts/LFS-build-step3.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 echo "LFS build - Step 3 - Create Basic Filesystem"
 CDIR=`pwd`
-cd $LFS
-mkdir -pv $LFS/{etc,var,sources,tools} $LFS/usr/{bin,lib,sbin} $LFS/lib/x86_64-linux-gnu
+
+mkdir -pv $LFS/{etc,var,sources} $LFS/usr/{bin,lib,sbin}
 cp -rvf $CDIR/sources/ $LFS
 cp -v $CDIR/Scripts/LFS-chroot-build-step{1,2}.sh $LFS
 chmod -v a+wt $LFS/sources
 
+cd $LFS
 for i in bin lib sbin; do
-	ln -sv usr/$i $LFS/$i
+	ln -sv usr/$i $LFS/$i;
 done
 
 case $(uname -m) in
 	x86_64) mkdir -pv $LFS/lib64 ;;
 esac
+
+mkdir -pv $LFS/tools
--- Scripts/LFS-build-step7.sh
+++ Scripts/LFS-build-step7.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-LFS=/mnt/lfs
-LFS_TGT=$(uname -m)-lfs-linux-gnu
 cd $LFS
 echo "LFS Build - Step 7 - Entering the chroot environment"
 chroot "$LFS" /usr/bin/env -i HOME=/root TERM="$TERM" PS1='(lfs chroot) \u:\w\$' PATH=/usr/bin:/usr/sbin /bin/bash --login
