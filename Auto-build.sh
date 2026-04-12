0cf290bf11da7bd8556a3dfc9f8ddfcf81dbffa6
Jackie Mann <jackiemann82@gmail.com>
2026-04-12 15:10:46 -0500

    Beta testing complete.

--- Auto-build.sh
+++ Auto-build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-LFS="/mnt/lfs"
-#Scripts/LFS-build-steps1-2.sh
-#Scripts/LFS-build-step3.sh
-#Scripts/LFS-build-step4.sh
-#su - lfs
-#Scripts/LFS-build-step6.sh
+Scripts/LFS-build-steps1-2.sh
+Scripts/LFS-build-step3.sh
+Scripts/LFS-build-step4.sh
+su - lfs
+Scripts/LFS-build-step6.sh
 Scripts/LFS-build-step7.sh
 exit 0
